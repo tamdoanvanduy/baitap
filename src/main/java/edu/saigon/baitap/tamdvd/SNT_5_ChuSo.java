465cdcbd6ac9325ba4189a74ae19ece3727fd5eb
Tâm <tamxu17@gmail.com>
2019-05-05 01:11:35 +0700

    baitap

---
+++ src/main/java/edu/saigon/baitap/tamdvd/SNT_5_ChuSo.java
@@ -0,0 +1,20 @@
+package edu.saigon.baitap.tamdvd;
+
+import java.util.Scanner;
+
+public class SNT_5_ChuSo {
+    public static void main(String[] args) {
+        SNT_5_chuSo();
+
+
+    }
+    public static void SNT_5_chuSo()
+    {
+        System.out.println("Số nguyên tố có 5 chữ số là: ");
+        for(int i = 10000; i<= 99999; i++)
+        {
+            if(SoNguyenTo.KiemTra_SNT(i))
+                System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/edu/saigon/baitap/tamdvd/n_SoNguyenTo_first.java
@@ -0,0 +1,31 @@
+package edu.saigon.baitap.tamdvd;
+
+import java.util.Scanner;
+
+public class n_SoNguyenTo_first {
+    private SoNguyenTo connect;
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập n phần tử cần liệt kê: ");
+        n = sc.nextInt();
+        n_SNT_First(n);
+
+    }
+    public static void n_SNT_First(int n)
+    {
+        System.out.println(n + " số nguyên tố đầu tiên là: ");
+        int dem = 0;
+        for(int i =2;;i++)
+        {
+            if(SoNguyenTo.KiemTra_SNT(i))
+            {
+                System.out.println(i);
+                dem++;
+            }
+            if(dem==n)
+                break;
+        }
+
+    }
+}
